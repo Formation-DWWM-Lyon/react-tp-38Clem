b2743cb26621738b5183ddfed8bf6454d1f5f123
Clemence <38Clem@users.noreply.github.com>
2020-03-05 09:50:44 +0100

    Add new path to UserList + Button on Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Home } from './Component';
+import { Home, UserList } from './Component';
 
 function App() {
   return (
       <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/userlist" component={UserList}/>
       </Switch>
       </BrowserRouter>
   );
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Layout } from '.'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <Layout>
         <h1>Mon super réseau social</h1>
+        <Button>
+          <Link to={`/userlist`}>
+          Go to Users List
+          </Link>
+          </Button>
       </Layout>
     </div>)
 
