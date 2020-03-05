b675871e34e5951a6fb38dd53d9da61709c1ca81
Clemence <38Clem@users.noreply.github.com>
2020-03-05 10:32:46 +0100

    Render Random Users List

--- src/Component/UserList.jsx
+++ src/Component/UserList.jsx
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import Layout from './Layout';
+import { ListGroup } from 'react-bootstrap';
+import Loader from 'react-loader-spinner';
 
 export default class UserList extends Component {
 
   state = {
-    user: null
+    result: null,
+    info: null,
   }
 
   getRandomUser = () =>
-  Axios.get(`https://randomuser.me/api/`)
-  .then((user)=>{console.log(user);
-  })
+    Axios.get(`https://randomuser.me/api/?results=10`)
+      .then((response) => this.setState({ result: response.data.results, info: response.data.info }))
+
+  componentDidMount = () => {
+    this.getRandomUser();
+  }
 
   render = () => {
+    const { result, info } = this.state;
+    console.log(result);
+
+    if (!result) {
+      return (
+        <div className="text-center">
+          <Loader
+            type="Puff"
+            color="black"
+            height={100}
+            width={100}
+          />
+        </div>
+      )
+    }
+
     return (
       <Layout>
+        <ListGroup>
+          {result.map((item, index) =>
+            <ListGroup.Item key={index}>
+              '{item.name.title} {item.name.first} {item.name.last}'
+            </ListGroup.Item>
+          )}
+        </ListGroup>
         <div>UserList Component</div>
       </Layout>
-      
-      ) 
+
+    )
   }
-  
+
 }
