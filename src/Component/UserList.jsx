42f9734137e23875a55a24c9befcf16d4f5bd119
Clemence <38Clem@users.noreply.github.com>
2020-03-05 16:44:41 +0100

    Last commit of the day, brain is dead

--- src/Component/UserList.jsx
+++ src/Component/UserList.jsx
@@ -5,13 +5,26 @@ import { ListGroup, Card, CardColumns } from 'react-bootstrap';
 import Loader from 'react-loader-spinner';
 import { Button } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
+import { Profile } from '.';
+import { Redirect } from 'react-router-dom';
 
 export default class UserList extends Component {
 
   state = {
     result: null,
     info: null,
+    index: null,
+    redirect: false,
   }
+
+  fetchProfile = () => {
+    console.log('Le click marche');
+    // const { result, info } = this.state;
+
+
+    this.setState({redirect:true})
+    return <Redirect to={'./profile/'} {...this.state}/>
+  };
 
   getRandomUser = () =>
     Axios.get(`https://randomuser.me/api/?results=10`)
@@ -22,11 +35,7 @@ export default class UserList extends Component {
   }
 
   render = () => {
-    const { result, info } = this.state;
-    console.log('result');
-    console.log(result);
-    console.log('info');
-    console.log(info);
+    const { result, info, redirect } = this.state;
     if (!result) {
       return (
         <div className="text-center">
@@ -40,20 +49,24 @@ export default class UserList extends Component {
       )
     }
 
+
     return (
       <Layout>
-        <CardColumns className="cardlist">
+        <CardColumns>
           {result.map((item, index) =>
-            <Card key={index}>
-              <Image className="ProfilPic" src={item.picture.large} />
-              <Card.Title className="Name">
-                {item.name.first} {item.name.last}
-              </Card.Title>
+            <Card key={index} >
+              <section className="text-center">
+                <Image className="card-img-top, rounded-lg" src={item.picture.medium} />
+                <Card.Title className="Name">
+                  {item.name.first} {item.name.last}
+                </Card.Title>
+                <Button onClick={this.fetchProfile}>See Profile</Button>
+              </section>
               <Card.Body>
                 <ListGroup >
-                  <Card.Text >{item.gender}</Card.Text>
-                  <Card.Text>{item.dob.age}</Card.Text>
-                  <Card.Text>{item.location.city}, {item.location.country}</Card.Text>
+                  <ListGroup.Item >{item.gender}</ListGroup.Item>
+                  <ListGroup.Item>{item.dob.age}</ListGroup.Item>
+                  <ListGroup.Item>{item.location.city}, {item.location.country}</ListGroup.Item>
                 </ListGroup>
               </Card.Body>
             </Card>
